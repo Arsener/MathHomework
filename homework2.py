3d3747d368010bb17985bea5f44b586ebd97f99e
Arsener <1769879278@qq.com>
2019-10-25 15:14:46 +0800

    '无穷范数'

--- homework2.py
+++ homework2.py
@@ -5,6 +5,7 @@ P91-1
 import numpy as np
 
 
+# 将矩阵A拆分成A = D - L - U
 def divide_matrix(H):
     n = H.shape[0]
     D = np.mat([[H[i, j] if i == j else 0 for j in range(n)] for i in range(n)])
@@ -13,12 +14,15 @@ def divide_matrix(H):
     return D, L, U
 
 
+# 生成n*n的Hilbert矩阵
 def hilbert_martrix(n):
     H = [[1 / (i + j - 1) for j in range(1, n + 1)] for i in range(1, 1 + n)]
     return np.mat(H)
 
 
+# Jacobi迭代法
 def jacobi(H, b, k, x=None):
+    # 初始向量为零向量
     if x is None:
         x = np.zeros(b.shape)
 
@@ -32,7 +36,9 @@ def jacobi(H, b, k, x=None):
     return x
 
 
+# SOR迭代法
 def sor(H, b, k, w=1., x=None):
+    # 初始向量为零向量
     if x is None:
         x = np.zeros(b.shape)
 
@@ -47,10 +53,14 @@ def sor(H, b, k, w=1., x=None):
 
 
 if __name__ == '__main__':
-    H = hilbert_martrix(6)
-    print(H)
-    b = np.sum(H, axis=1)
-    x = jacobi(H, b, 5)
-    print(x)
-    x = sor(H, b, 15000)
-    print(x)
+    n = 6
+    H = hilbert_martrix(n)
+    print('Hilbert Matrix:\n{}'.format(H))
+    x = np.ones((H.shape[0], 1))
+    b = H * x
+    x_j = jacobi(H, b, 10)
+    print('Jacobi:\n{}'.format(x_j))
+    x_s = sor(H, b, 1000, w=1.)
+    print('SOR:\n{}'.format(x_s))
+    print('Infinite norm of (x - x_s):')
+    print(np.linalg.norm(x - x_s, ord=np.inf))
