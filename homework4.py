3c182f457b470bf1f5e57460dfaeb7dca60d5fcc
Arsener <1769879278@qq.com>
2019-11-28 00:37:47 +0800

    '追赶法'

--- homework4.py
+++ homework4.py
@@ -5,9 +5,8 @@ P190-2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.interpolate import *
 
-
+# 拉格朗日插值
 def Lagrange(x, fx):
     f_A = []
     n = len(x)
@@ -30,41 +29,11 @@ def Lagrange(x, fx):
 
         return ans
 
+    # 返回函数表达式
     return polynomial
 
 
-def solve(A_, b):
-    # 增广矩阵
-    A = np.concatenate((A_, b), axis=1)
-    # print(A)
-    size = A.shape[0]
-
-    import copy
-    # 消元
-    for i in range(size - 1):
-        # 找到主元
-        k = np.argmax(abs(A[i:size, i]))
-        # 交换行
-        if k != 0:
-            tmp = copy.copy(A[k + i])
-            A[k + i] = copy.copy(A[i])
-            A[i] = copy.copy(tmp)
-
-        # 作变换
-        for j in range(i + 1, size):
-            t = A[j, i] / A[i, i]
-            A[j, i:] -= A[i, i:] * t
-
-    # 创建一个和b的size相同的矩阵，作为解
-    x = np.mat(np.zeros(b.shape), dtype=np.float64)
-    # 回代计算
-    i = size - 1
-    while i >= 0:
-        x[i, 0] = (A[i, size] - A[i, i + 1:size] * x[i + 1:, 0]) / A[i, i]
-        i -= 1
-
-    return x
-
+# 三次样条插值
 def spline_II_edge(x, fx, M_0=0., M_n=0.):
     n = len(x)
     # h
@@ -74,14 +43,6 @@ def spline_II_edge(x, fx, M_0=0., M_n=0.):
     # lambda
     lmbd = [1 - m for m in mu]
 
-    # 三对角矩阵
-    Mat = np.eye(n-2) * 2
-    for i in range(n - 2):
-        if i - 1 >= 0:
-            Mat[i][i - 1] = mu[i]
-        if i + 1 < n - 2:
-            Mat[i][i + 1] = lmbd[i]
-
     # 1阶均差
     f_1_step = [(fx[i] - fx[i - 1]) / (x[i] - x[i - 1]) for i in range(1, n)]
     # 2阶均差 * 6
@@ -90,9 +51,24 @@ def spline_II_edge(x, fx, M_0=0., M_n=0.):
     f_2_step[0] -= mu[0] * M_0
     f_2_step[-1] -= lmbd[-1] * M_n
 
-    # TODO
-    M = [M_0] + [i[0] for i in solve(np.mat(Mat), np.mat(f_2_step).T).tolist()] + [M_n]
-    print(M)
+    # 追赶法求解三对角矩阵：AM = f_2_step
+    # A = LU
+    u = [2]
+    l = []
+    for i in range(1, n - 2):
+        l.append(mu[i] / u[-1])
+        u.append(2 - l[-1] * lmbd[i - 1])
+
+    # Ly = f_2_step
+    y = [f_2_step[0]]
+    for i in range(1, n - 2):
+        y.append(f_2_step[i] - l[i - 1] * y[-1])
+
+    # UM = y
+    M = [y[-1] / u[-1]]
+    for i in range(n - 4, -1, -1):
+        M = [1 / u[i] * (y[i] - lmbd[i] * M[0])] + M
+    M = [M_0] + M + [M_n]
 
     def polynomial(new_x):
         if not x[0] <= new_x <= x[-1]:
@@ -105,16 +81,8 @@ def spline_II_edge(x, fx, M_0=0., M_n=0.):
                 ans += (fx[i + 1] - (M[i + 1] * h[i] * h[i]) / 6) * (new_x - x[i]) / h[i]
                 return ans
 
+    # 返回函数表达式
     return polynomial
-
-
-def f(x):
-    if x <= 1.:
-        return 0.5 * x ** 3 - 0.15 * x ** 2 + 0.15 * x
-    elif x <= 2:
-        return -1.2 * (x - 1) ** 3 + 1.35 * (x - 1) ** 2 + 1.35 * (x - 1) + 0.5
-    elif x <= 3:
-        return 1.3 * (x - 2) ** 3 - 2.25 * (x - 2) ** 2 + 0.45 * (x - 2) + 2.
 
 
 if __name__ == '__main__':
@@ -128,9 +96,10 @@ if __name__ == '__main__':
     spline_x = [spline(i) for i in t]
     lagrange_x = [lagrange(i) for i in t]
 
-    plt.plot(x, fx, color='m', linestyle='', marker='o')
-    plt.plot(t, lagrange_x, color='b', linestyle='-', marker='', label="Lagrange")
-    plt.plot(t, spline_x, color='r', linestyle='-', marker='', label="Spline")
+    plt.scatter(x, fx, color='g', marker='o')
+    plt.plot(t, lagrange_x, color='b', linestyle='-', label="Lagrange")
+    plt.plot(t, spline_x, color='r', linestyle='-', label="Spline")
+
     plt.legend(loc='best')
 
     plt.show()
